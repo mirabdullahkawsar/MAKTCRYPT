02ed7f1be6fad5040dae19edde1066c9d9cc590b
mirabdullahkawsar <ttonmoy01@gmail.com>
2025-04-29 06:33:06 +0600

    Fix start script to point to server mongodb

--- server/server.js
+++ server/server.js
@@ -1,3 +1,39 @@
+// // server/server.js
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/auth");
+// const postRoutes = require("./routes/post");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/posts", postRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("✅ Backend is running securely.");
+//   });
+  
+
+// // MongoDB connection
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("MongoDB connected");
+//     app.listen(process.env.PORT || 5000, () =>
+//       console.log("Server running on port 5000")
+//     );
+//   })
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
 // server/server.js
 require("dotenv").config();
 const express = require("express");
@@ -17,12 +53,11 @@ app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
     res.send("✅ Backend is running securely.");
-  });
-  
+});
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URI, { // Ensure this variable is set
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
